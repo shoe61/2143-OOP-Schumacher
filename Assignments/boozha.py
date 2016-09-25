c8667508aad907beaddc22ef1991ffbd1aaec451
Scott Schumacher <scottmachershoe@yahoo.com>
2016-09-24 23:57:46 -0500

    added fraction_class.py

---
+++ Assignments/boozha.py
@@ -0,0 +1,54 @@
+class fraction(object):
+
+    def __init__(self,wh=None,n=None,d=None):
+        if wh:
+            self.whole = wh
+        else:
+            self.whole = 0.0
+        self.numerator = n + self.whole * d
+        self.denominator = d
+        self.reduce()
+    
+
+    def reduce(self):
+        thegcd = self.gcd(self.numerator,self.denominator)
+        self.numerator =int(self.numerator / thegcd)
+        self.denominator = int (self.denominator / thegcd)
+        if self.numerator > self.denominator:
+            self.whole = int(self.numerator / self.denominator)
+            self.numerator %= self.denominator
+
+        
+        
+    def gcd(self, a, numerator):
+        
+        while numerator:      
+            a, numerator = numerator, a % numerator
+        return a
+    
+    
+    def __str__(self):
+        if self.whole  > 0:
+            return "%d -  %d / %d" % (self.whole,self.numerator,self.denominator)
+        else: 
+            return "%d / %d" %(self.numerator,self.denominator)
+
+    def __add__(self,rhs):
+        denominator = int(self.denominator * rhs.denominator)
+        numerator = int((self.numerator + int(self.denominator * self.whole)) * rhs.denominator) + int((rhs.numerator + int(rhs.denominator * rhs.whole)) * self.denominator)
+        if(numerator == denominator):
+            return 1
+        else:
+            return fraction(0, numerator,denominator)
+
+
+
+
+
+F = fraction(1,1,3)
+G = fraction(1,1,4)
+
+print('F: ', F, '  G: ', G)
+
+H = F + G
+print('H = ', H)
